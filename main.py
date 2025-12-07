0b95ca6f94e71cf5e31151d0091593dffdb1ef94
M_Chrn_W <91328364+MishaChrn@users.noreply.github.com>
2025-12-08 04:44:25 +0900

    first commit that includes a summary of this app

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+"""
+This is a tank quiz app.
+What can you do with this app:
+1. Choose a quiz category to challenge in the table of contents.
+2. Tank Appearance Match: See a picture of a tank and choose a correct tank name and its nation.
+3. Tank Role Match: See a tank name and choose a correct tank class.
+4. Tank Gameplay Match: See a tank name and choose a correct gameplay description.
+5. Tank Trivia: Choose a tank to read about its history.
+"""
+
