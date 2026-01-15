cd02acfd1a363541ab662e324f1419151334a23b
M_Chrn_W <91328364+MishaChrn@users.noreply.github.com>
2026-01-16 04:07:45 +0900

    Changes on Jan 16: making def test_read_all_quiz_answers() in test_quiz_functions file

--- test_quiz_functions.py
+++ test_quiz_functions.py
@@ -31,13 +31,22 @@ def test_tank_role_match_quiz_answers():
     tank_appearance_quiz_answers_dictionary = tank_appearance_quiz_answers_reader.to_dict() # Modifies the dataframe to the dict type.
     assert tank_appearance_quiz_answers_dictionary["Tank Name"][0] == "KV-2" and tank_appearance_quiz_answers_dictionary["Tank Class"][0] == "HT"
 
+# def test_read_all_quiz_answers():
+#     # Reads all answer sets and store them in a dictionary.
+#     tank_quiz_answers = "tank_quiz_answers.csv"
+#     with open(tank_quiz_answers, "r") as csvfile:
+#         tank_appearance_quiz_answers_reader = pd.read_csv(csvfile)
+#     all_tank_quiz_answers_dictionary = tank_appearance_quiz_answers_reader.to_dict(orient='records') # Modifies the dataframe to the dict type by each row, using the column name as each key.
+#     assert all_tank_quiz_answers_dictionary #???
+
 def test_read_all_quiz_answers():
     # Reads all answer sets and store them in a dictionary.
     tank_quiz_answers = "tank_quiz_answers.csv"
     with open(tank_quiz_answers, "r") as csvfile:
-        tank_appearance_quiz_answers_reader = pd.read_csv(csvfile, usecols=["Tank Name", "Tank Class"])
-    all_tank_quiz_answers_dictionary = tank_appearance_quiz_answers_reader.to_dict(orient='records') # Modifies the dataframe to the dict type by each row, using the column name as each key.
-    assert all_tank_quiz_answers_dictionary #???
+        tank_appearance_quiz_answers_reader = pd.read_csv(csvfile).set_index("Pic ID", inplace=True)
+        # Where to put .set_index("Pic ID", inplace=True)??
+    all_tank_quiz_answers_dictionary = tank_appearance_quiz_answers_reader.to_dict(orient='index') # Modifies the dataframe to the dict type by each row, using the column name as each key.
+    assert all_tank_quiz_answers_dictionary["'ussr_kv_2"]["Tank Name"] == "KV-2"
 
 """
 一行一意になるはずなので、
