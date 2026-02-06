26a0d0f941ae4678d0287bdc08db31dec59a95c8
M_Chrn_W <91328364+MishaChrn@users.noreply.github.com>
2026-02-07 08:31:47 +0900

    mock data were added to the answer table, Sherman Easy 8 was renamed to M4A3E8, main flow was roughly described in quiz_function.py

--- quiz_functions.py
+++ quiz_functions.py
@@ -2,5 +2,20 @@ import csv
 import yaml
 
 
+"""
+flow:
+quiz_selection()
 
+input("Select a quiz: ")
 
+create_random_tank_appearance_quiz()
+
+input("Answer: ")
+
+check_player_answer(player_answer, expected_answer)
+"""
+
+"""
+pictures and trivias referenced Wikipedia
+game play definitions referenced Blitz Hangar
+"""
