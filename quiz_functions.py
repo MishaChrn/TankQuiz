f10b0ed2ab67385e960e892351fc57514d584d55
M_Chrn_W <91328364+MishaChrn@users.noreply.github.com>
2026-03-24 03:05:14 +0900

    def create_random_answer_dict is finished, make a equivalent test code

--- quiz_functions.py
+++ quiz_functions.py
@@ -36,12 +36,16 @@ def show_quiz(random_question_key_set):
     return random_quiz_value
 
 def create_random_answer_dict(random_question_key_set, random_quiz_value):
-    random_answer_set = set() #Should be dict
-    for question_key in range(3):
-        random_answer_set.add(random_quiz_value[question_key]["Tank Name"])
+    random_answer_set = set()
+    random_answer_dict = dict()
+    for question_option in range(3):
+        random_answer_set.add(random_quiz_value[question_option]["Tank Name"])
     else:
         random_answer_set.add(random_quiz_value["Tank Name"])
-    print(random_answer_set)
+    for question_key in range(random_answer_set):
+        random_answer_dict[question_key] = random_answer_set.pop()
+    else:
+        print(random_answer_dict)
 
 # I don't want to list up if-elif statements in this function
 def check_correct_answer(random_quiz_value, player_answer, player_input):
