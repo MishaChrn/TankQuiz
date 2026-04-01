ef26ef31ea084f427da9ba0e221fba5fc0c3fd45
M_Chrn_W <91328364+MishaChrn@users.noreply.github.com>
2026-04-02 03:42:04 +0900

    making def test_create_random_answer_dict, fixed the issue of incorrect parameter pairs, fix the iteration expression with set

--- test_quiz_functions.py
+++ test_quiz_functions.py
@@ -27,8 +27,16 @@ def test_show_quiz(random_question_key_set, expected_value):
 
 @pytest.mark.parametrize(
     "random_question_key_set, random_quiz_value, expected_answer_dict_value",
-    [([{"Pic ID": "ussr_kv_2", "Tank Name": "KV-2"}, {"Pic ID": "usa_m4a3e8", "Tank Name": "M4A3E8"}, {"Pic ID": "ussr_is_3", "Tank Name": "IS-3"}, {"Pic ID": "germany_leopard_1", "Tank Name": "Leopard 1"}],
-    ["KV-2", "M4A3E8", "IS-3", "Leopard 1"])]
+    [([{"Pic ID": "ussr_kv_2", "Tank Name": "KV-2"}, {"Pic ID": "usa_m4a3e8", "Tank Name": "M4A3E8"},
+       {"Pic ID": "ussr_is_3", "Tank Name": "IS-3"}, {"Pic ID": "germany_leopard_1", "Tank Name": "Leopard 1"}],
+      {"Pic ID": "ussr_kv_2", "Tank Name": "KV-2"}, ["KV-2", "M4A3E8", "IS-3", "Leopard 1"]),
+     ([{"Pic ID": "ussr_kv_2", "Tank Name": "KV-2"}, {"Pic ID": "usa_m4a3e8", "Tank Name": "M4A3E8"},
+       {"Pic ID": "ussr_is_3", "Tank Name": "IS-3"}, {"Pic ID": "germany_leopard_1", "Tank Name": "Leopard 1"}],
+      {"Pic ID": "germany_leopard_1", "Tank Name": "Leopard 1"}, ["KV-2", "M4A3E8", "IS-3", "Leopard 1"]),
+     ([{"Pic ID": "ussr_kv_2", "Tank Name": "KV-2"}, {"Pic ID": "usa_m4a3e8", "Tank Name": "M4A3E8"},
+       {"Pic ID": "ussr_is_3", "Tank Name": "IS-3"}, {"Pic ID": "germany_leopard_1", "Tank Name": "Leopard 1"},
+       {"Pic ID": "usa_m_iii_yoh", "Tank Name": "M-III-Yoh"}],
+      {"Pic ID": "germany_leopard_1", "Tank Name": "Leopard 1"}, ["KV-2", "M4A3E8", "IS-3", "Leopard 1"])]
 )
 def test_create_random_answer_dict(random_question_key_set, random_quiz_value, expected_answer_dict_value):
     random_answer_set = set()
